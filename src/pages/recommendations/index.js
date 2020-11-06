a9b71fae1a7d87e660a26d5cac8bcd4997b78863
Jenny Schutzman <Jschutzman@stempremier.com>
2020-11-06 13:58:55 -0500

    recommendations component now pulls all tours, receives the state of finalOptions array from intesrsets, and started handleClick to do the master filter

--- src/pages/recommendations/index.js
+++ src/pages/recommendations/index.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import { contains } from 'ramda';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import { getTours } from '../actions/allToursActions'
+import * as R from 'ramda'
 
 const Recommendations = () => {
-  // const [allTours, setAllTours] = useState([]);
+  const [allTours, setAllTours] = useState([]);
+
   // const [recommendations, setRecommendations] = useState([])
   const location = useLocation();
   const history = useHistory();
 
-  // useEffect(() => {
-  //   getTours()
-  // }, [])
+  useEffect(() => {
+    fetch('http://localhost:3001/allTours')
+      .then(res => res.json())
+      .then(data => setAllTours(data))
+  }, [])
 
-  // console.log('')
-  console.log('locatoin', location)
-  console.log('history', history)
+  useEffect(() => {
+    getRecommendations()
+  }, [allTours])
+
+  const tags = location.state
+
+  const getRecommendations = () => {
+
+    // const hasMatch = (value) => value.con
+
+    // const checkMatches = (array, target) => target.every(value => array.includes(value))
+
+    // const list = allTours.filter(tour => checkMatches(tags, tour.options))
+
+    // const list = diff()
+
+    c      var resultOptions = []
+
+    console.log(
+      'action.payload.interests',
+      JSON.stringify(action.payload.interests)
+    )
+    const chosenInterest = find(i => i.selected, action.payload.interests)
+    //console.log('chosenInterests', JSON.stringify(chosenInterest))
+    const resultOptions1 = concat([chosenInterest.name], resultOptions)
+    //console.log('resultOptions1', JSON.stringify(resultOptions1))
+    const chosenExp = find(e => e.selected, chosenInterest.experienceTypes)
+    const resultOptions2 = concat([chosenExp.name], resultOptions1)
+    const chosenDays = find(d => d.selected, action.payload.dow)
+    const resultOptionsFINAL = concat([chosenDays.name], resultOptions2)
+    return (state = resultOptionsFINAL)
+    console.log('list', list)
+  }
+
   return (
     <h1>Recommendations page</h1>
   )
 }
-
-// export default connect(null, { getTours })(Recommendations)
 
 export default Recommendations
